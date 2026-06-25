561a8f8669497d32014d07ee95b7a73df2b9df52
geyclube <120900666+geyclube@users.noreply.github.com>
2026-06-25 09:23:32 +0500

    Патриотичный гайдбук

--- runserver-Release.sh
+++ runserver-Release.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-dotnet run --project Content.Server --configuration Release
+dotnet run --project Content.Server --configuration Release --config-file server_config.toml
 read -p "Press enter to continue"
